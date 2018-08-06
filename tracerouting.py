b859d78db3de45bda223dc57e943ecdff8c7cb0f
see <5see@informatik.uni-hamburg.de>
2018-08-06 12:25:07 +0200

    refactoring for all projects

--- tracerouting.py
+++ tracerouting.py
@@ -2,16 +2,10 @@ import vpnParser
 import vpnConnector
 import time
 import subprocess
-#Today:
-#Abgegriffene Daten Analysieren
-#Own Database / Lookup Sites?
-#Sutruktur BA verfeinern
 
 
 
-def build_ip_list(prefix, ip):
-    pass
-
+#TODO vielleicht ip mapping based on my database
 def traceroute(ip):
     proc = subprocess.Popen(["sudo", "traceroute", "-T", "-n", "-A", ip], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     stdout, _ = proc.communicate()
@@ -30,27 +24,6 @@ def parse_asn_path(traceroute_output, ip):
     print("[+] Path taken to",ip,":", asn_path)
     return asn_path
 
-
-
-def validate_path(taken_path, links):
-    if links in taken_path:
-        pass
-
-
-#Policen überlegen
-def validate_ip_path(taken_asn_path, announced_asn_path,  missing_link_list, prefix_owners):
-
-    """
-    Validates the ip path taken vs. the the announced path.
-    Needs research!!!
-    :param taken_asn_path: list of asn (path taken)
-    :param announced_asn_path list: of asn (path announced)
-    :param missing_link_list: list of asn tuples representing a link. [[ip1,ip2],[ip2,ip3],[ip4,ip5]]
-    :param prefix_owners: list of ips that should be owning the prefix
-    :returns
-    :returns
-    """
-    pass
 
 
 if __name__ == '__main__':
